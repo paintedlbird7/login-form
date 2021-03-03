d2b370d19a781e18cfb8b048e3c0ea5a89ed201b
paintedlbird7 <rosaperezlarios@gmail.com>
2021-03-03 11:51:43 -0800

    built form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function Form(props){
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    alert(`Submitting Email ${email}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type='text'
+          placeholder='Email'
+          keyboardType='email-address'
+          autoCapitalize='none'
+          value={email}
+          onChangeText={setEmail}
+        />
+        <input
+          placeholder={'Password'}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+      </label>
+      <input type='submit' value='Submit' />
+    </form>
   );
-}
-
-export default App;
+};
