bb0509fc847b525254436b17564cfc1b956143ff
paintedlbird7 <rosaperezlarios@gmail.com>
2021-03-11 13:41:21 -0800

    tracks inputs (on the screen)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,12 @@
 import React, { useState } from 'react';
 
-export default function Form(props) {
-  const [submitted, setSubmitted] = useState(false);
-  const [inputs, setInputs] = React.useState({
-    email: '',
-    password: '',
-  });
+export default function Form() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   // event handler that controls form’s submission
   const handleSubmit = e => {
     e.preventDefault();
-    setSubmitted(true);
-    alert(`Submitting Email & Password ${inputs}`);
-    console.log(inputs);
-  };
-
-  // handler that changes the input element’s value
-  const handleEmailInputChange = e => {
-    // e.persists();
-    setInputs(inputs => ({
-      ...inputs,
-      email: e.target.value,
-    }));
-  };
-
-  // handler that changes the input element’s value
-  const handlePasswordInputChange = e => {
-    // e.persists();
-    setInputs(inputs => ({
-      ...inputs,
-      password: e.target.value,
-    }));
   };
 
   return (
@@ -41,20 +17,24 @@ export default function Form(props) {
           placeholder='Email'
           keyboardType='email-address'
           autoCapitalize='none'
-          value={inputs.email}
-          onChange={handleEmailInputChange}
+          value={email}
+          onChange={e => 
+            setEmail(e.target.value)}
           required
         />
         <input
-          placeholder={'Inputs'}
-          value={inputs.password}
-          onChange={handlePasswordInputChange}
-          secureTextEntry
+          placeholder='Password'
+          keyboardType='password'
+          autoCapitalize='none'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
           type='password' // secureTextEntry
           required
         />
       </label>
       <input type='submit' value='Submit' />
+      <p>{email}</p>
+      <p>{password}</p>
     </form>
   );
 }
